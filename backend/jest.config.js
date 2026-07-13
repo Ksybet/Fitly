cf210c57e46021df40733752a279270760d73a06
Artem <90620853+k3llone@users.noreply.github.com>
2026-07-13 18:29:00 +0300

    test: установить пороги покрытия Jest

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -4,8 +4,18 @@ module.exports = {
 	testPathIgnorePatterns: ['/node_modules/', '/test/integration/'],
 	collectCoverageFrom: [
 		'src/modules/**/*.service.js',
+		'src/modules/auth/auth.middleware.js',
 		'src/middlewares/**/*.js',
 		'src/config/env.js',
 		'src/utils/**/*.js',
+		'src/server.js',
 	],
+	coverageThreshold: {
+		global: {
+			statements: 85,
+			branches: 80,
+			functions: 80,
+			lines: 85,
+		},
+	},
 };
