4dc341b24373e7d9e84e8a6bcfd32d380494f549
Artem <90620853+k3llone@users.noreply.github.com>
2026-07-31 02:52:33 +0300

    refactor: подключить каталог тренировок к API
    Добавляет публичные контракты, API-клиент и совместимый с Expo SDK 54 тестовый контур. Каталог загружает все карточки с серверными фильтрами, пагинацией и состояниями загрузки и ошибок.

---
+++ frontend/jest.setup.js
@@ -0,0 +1,8 @@
+jest.mock(
+	'@react-native-async-storage/async-storage',
+	() => require(
+		'@react-native-async-storage/async-storage/jest/async-storage-mock',
+	),
+);
+
+globalThis.IS_REACT_ACT_ENVIRONMENT = true;
